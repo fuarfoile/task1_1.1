783b48395bf4328e80820b9fbe64d723cc5e73a1
Oleh Bondarenko <boa.st@ya.ru>
2017-09-04 00:13:45 +0300

    Upd

--- src/com/boast/Converter.java
+++ src/com/boast/Converter.java
@@ -1,5 +1,5 @@
 /*
- * Converter.java 24/07/2017
+ * Converter.java 04/09/2017
  *
  * Created by Bondarenko Oleh
  */
@@ -9,49 +9,87 @@ package com.boast;
 
 public class Converter {
     public static String decToBin(int number) {
-
-        String result = "";
-        while(number > 1) {
-            result = number % 2 + result;
-            number >>= 1;
+        long longNumber = number;
+        if (longNumber < 0) {
+            longNumber = longNumber + ((long) Integer.MAX_VALUE) * 2 + 2;
         }
 
-        return number + result;
+        StringBuilder result = new StringBuilder();
+        while(longNumber > 1) {
+            result.append(longNumber % 2);
+            longNumber >>= 1;
+        }
+        result.append(longNumber);
+
+        for (int i = 4; i < result.length(); i++) {
+            if ((i + 1) % 5 == 0) {
+                result.insert(i, ' ');
+            }
+        }
+
+        result.reverse();
+
+        return result.toString();
     }
 
     public static String decToOct(int number) {
-
-        String result = "";
-        while(number > 7) {
-            result = number % 8 + result;
-            number >>= 3;
+        long longNumber = number;
+        if (longNumber < 0) {
+            longNumber = longNumber + ((long) Integer.MAX_VALUE) * 2 + 2;
         }
 
-        return number + result;
+        StringBuilder result = new StringBuilder();
+        while(longNumber > 7) {
+            result.append(longNumber % 8);
+            longNumber >>= 3;
+        }
+        result.append(longNumber);
+
+        for (int i = 3; i < result.length(); i++) {
+            if ((i + 1) % 4 == 0) {
+                result.insert(i, ' ');
+            }
+        }
+
+        result.reverse();
+
+        return result.toString();
     }
 
     public static String decToHex(int number) {
+        long longNumber = number;
+        if (longNumber < 0) {
+            longNumber = longNumber + ((long) Integer.MAX_VALUE) * 2 + 2;
+        }
 
-        String result = "";
-        while (number > 15) {
+        StringBuilder result = new StringBuilder();
+        while (longNumber > 15) {
 
-            if (number % 16 > 9) {
-                result = (char) (number % 16 + 'a' - 10) + result;
+            if (longNumber % 16 > 9) {
+                result.append((char) (longNumber % 16 + 'a' - 10));
             }
             else {
-                result = number % 16 + result;
+                result.append(longNumber % 16);
             }
 
-            number >>= 4;
+            longNumber >>= 4;
         }
 
-        if (number % 16 > 9) {
-            result = (char) (number % 16 + 'a' - 10) + result;
+        if (longNumber % 16 > 9) {
+            result.append((char) (longNumber % 16 + 'a' - 10));
         }
         else {
-            result = number % 16 + result;
+            result.append(longNumber % 16);
         }
 
-        return result;
+        for (int i = 4; i < result.length(); i++) {
+            if ((i + 1) % 5 == 0) {
+                result.insert(i, ' ');
+            }
+        }
+
+        result.reverse();
+
+        return result.toString();
     }
 }
--- src/com/boast/task1/Main.java
+++ src/com/boast/task1/Main.java
@@ -14,6 +14,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.print("Enter number to convert: ");
+
         Scanner in = new Scanner(System.in);
         int number = in.nextInt();
 
